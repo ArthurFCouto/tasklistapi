a6e4856d2ca2cecfc2786603cb51b5e2312b990d
ArthurFCouto <arthurfcouto@gmail.com>
2022-03-17 14:53:58 -0300

    Correções do último commit

--- src/app/middlewares/notification.ts
+++ src/app/middlewares/notification.ts
@@ -7,22 +7,26 @@ export const myEmitter = new events.EventEmitter();
 const header = {
     'Content-Type': 'text/event-stream',
     'Connection': 'heep-alive',
+    'Access-Control-Allow-Origin': '*',
     'Cache-Control': 'no-cache',
 };
 
-const notificationMiddleware = async (req: any, res: Response, next: NextFunction) => {
+const notificationMiddleware = (req: any, res: Response, next: NextFunction) => {
     //Header setado para informar que é um SSE e para não gardar cache
     res.writeHead(200, header);
     try {
-        const notification = await Notification.findAll({
-            where: {
-                userId: req.userId
-            }
-        })
-            .then((list: any) => list.sort((prev: any, next: any) => prev.id === next.id ? 0 : prev.id > next.id ? 1 : -1));
-        const data = `{"title": ${notification[notification.length - 1].title}, "message": ${notification[notification.length - 1].message},}\n\n`;
-            myEmitter.on('new_notification', () => {
-            res.write(data);
+        myEmitter.on('new_notification', async () => {
+            const notification = await Notification.findAll({
+                where: {
+                    userId: req.userId
+                }
+            })
+                .then((list: any) => list.sort((prev: any, next: any) => prev.id === next.id ? 0 : prev.id > next.id ? 1 : -1));
+            const data = {
+                title: notification[notification.length - 1].title,
+                message: notification[notification.length - 1].message
+            };
+            res.write(JSON.stringify(data) + '\n\n');
         });
     } catch (error) {
         logger.error(error);
