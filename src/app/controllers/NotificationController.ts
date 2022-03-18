2df86de52abae684caa0993eb31d397bc3d58a9e
ArthurFCouto <arthurfcouto@gmail.com>
2022-03-18 16:10:08 -0300

    Reversão do ultimo commit

--- src/app/controllers/NotificationController.ts
+++ src/app/controllers/NotificationController.ts
@@ -1,6 +1,4 @@
 import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
-import User from '../../database/models/user';
 import logger from '../../logger';
 import Config from '../config';
 import NotificationService, { TypeNotification } from '../service/NotificationService';
@@ -10,31 +8,21 @@ const { headerEventStream } = Config;
 
 class NotificationController {
 
-    async getLast(req: Request, res: Response, next: NextFunction) {
+    getLast(req: any, res: Response, next: NextFunction) {
+        const { userId } = req.params;
         res.writeHead(200, headerEventStream);
-        const authHeader = req.headers.authorization;
-        if (authHeader) {
-            try {
-                const [, token] = authHeader.split(' ');
-                const decoded = Object(await jwt.verify(token, String(process.env.JWT_SECRET)));
-                const user = await User.findByPk(decoded.id);
-                if (user) {
-                    myEmitter.on('new_notification', async () => {
-                        const notification = await NotificationService.getLastNotification(decoded.id);
-                        const data = {
-                            title: notification?.title,
-                            message: notification?.message
-                        };
-                        res.write(`data: ${JSON.stringify(data)}\n\n`);
-                    });
-                } else
-                    return res.write(`data: { error: Token user not found, invalid token }`);
-            } catch (error) {
-                logger.error(error);
-                return res.status(500).json({ error: 'Error on our server. Try later' });
-            }
-        } else {
-            return res.status(401);
+        try {
+            myEmitter.on('new_notification', async () => {
+                const notification = await NotificationService.getLastNotification(userId);
+                const data = {
+                    title: notification?.title,
+                    message: notification?.message
+                };
+                res.write(`data: ${JSON.stringify(data)}\n\n`);
+            });
+        } catch (error) {
+            logger.error(error);
+            return res.status(500).json({ error: 'Error on our server. Try later' });
         }
     }
 
